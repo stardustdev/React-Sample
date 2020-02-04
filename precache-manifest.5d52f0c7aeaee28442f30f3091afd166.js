2d5b0f8ddd73f925a92838d4748ea8bbf2c767e8
stardustdev <stardustdev@outlook.com>
2020-02-05 03:36:42 +1000

    Updates

---
+++ precache-manifest.5d52f0c7aeaee28442f30f3091afd166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00170f237f36efa0e0c5a49d6a1a54f2",
+    "url": "/React-Sample/index.html"
+  },
+  {
+    "revision": "6d15130a600f49143c39",
+    "url": "/React-Sample/static/css/main.a265946f.chunk.css"
+  },
+  {
+    "revision": "46eba0bb8eee9f2ee5e2",
+    "url": "/React-Sample/static/js/2.a28d38e6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Sample/static/js/2.a28d38e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d15130a600f49143c39",
+    "url": "/React-Sample/static/js/main.414c67ab.chunk.js"
+  },
+  {
+    "revision": "7239f6943f2cf993c38b",
+    "url": "/React-Sample/static/js/runtime-main.eb858fe8.js"
+  }
+]);
